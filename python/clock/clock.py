6c6fe5407afc4c8b58b3bb965ef38e56eb27d37e
Victoria Betts <victoriabetts@Perseus.local>
2018-03-08 15:57:07 -0500

    Add first stage of solution for Clocks problem

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,3 +1,14 @@
-def Clock(hours, minutes):
+import math
 
+def Clock(hours=0, minutes=0):
+
+    mins = (hours*60) + minutes;
+
+    minRemainder = mins % 60
+
+    hours = (mins - minRemainder)/60
+
+    timeH = int(hours % 24)
+
+    return str(timeH).zfill(2) + ":" + str(minRemainder).zfill(2)
 
