625f9ad15f54a3d7ec119e4ff0665f9fdba153e8
Victoria Betts <victoriabetts@perseus.lan>
2018-03-09 11:28:48 -0500

    Solution for rna-transcription

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,9 @@
 def to_rna(dna_strand):
-    pass
+    rna = {"G":"C", "C":"G", "T":"A", "A":"U"}
+    result = ""
+    for n in dna_strand:
+        if n in rna:
+            result += rna[n]
+        else:
+            raise Exception("ValueError: " + n + " is not a valid nucleotide.")
+    return result
