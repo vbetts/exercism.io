647da95e8d3c3558f16a5dbf4a5b5644c5269cc3
Victoria Betts <victoriabetts@perseus.lan>
2018-03-09 11:59:21 -0500

    Added alternate sollution for reversing string in place.

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -1,3 +1,5 @@
+# This works if you need to keep the original string
+
 def reverse(input=''):
     if input == '':
         return ''
@@ -6,5 +8,20 @@ def reverse(input=''):
     for l in input:
         result += input[i]
         i-=1
-
     return result
+
+# You can't reverse the string 'in place' in python because strings are immutable
+# but if you could, this is a more efficient solution to the problem
+
+#def reverse(input=''):
+#    if input == '':
+#        return ''
+#    i = 0
+#    r = (len(input)-1)/2
+#    while i <= r:
+#        lastIndex = len(input)-1-i
+#        lastLetter = input[lastIndex]
+#        input[input[lastIndex] = input[i]
+#        input[i] = lastLetter
+#        i+= 1
+#    return input
