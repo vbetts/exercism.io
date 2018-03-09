500aaf81e31c3780a9f73869f5794930f3ed8dc1
Victoria Betts <victoriabetts@perseus.lan>
2018-03-09 11:30:27 -0500

    Raise value error instead of exception

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -5,5 +5,5 @@ def to_rna(dna_strand):
         if n in rna:
             result += rna[n]
         else:
-            raise Exception("ValueError: " + n + " is not a valid nucleotide.")
+            raise ValueError(n + " is not a valid nucleotide.")
     return result
