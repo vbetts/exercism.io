6d1f61f44cc86cb6cc0886b7cf4c68d9309c103a
Victoria Betts <toribetts@gmail.com>
2018-03-22 15:15:54 -0400

    Solution for isbn10 verifier problem

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,17 @@
+import re
+
 def verify(isbn):
-    pass
+    isbn = isbn.strip().replace("-", "").replace(" ", "").lower()
+    #check if the string is empty, not the right length, or contains an invalid character
+    if (isbn=="") or not(len(isbn)==10) or not(re.match("^[0-9]{9}[0-9xX]$",isbn)):
+        return False
+    i = 0
+    total = 0
+    for x in range(len(isbn), 0, -1):
+        if isbn[i] == "x" or isbn[i] == "X":
+            y = 10
+        else:
+            y = int(isbn[i])
+        total+=y*x
+        i+=1
+    return (total%11 ==0)
